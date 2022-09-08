b050396a3dcffc98c52c4ba7296cc93111cb4644
bakhtechsolutions <solutions@bakhtech.com.ng>
2022-09-08 09:47:46 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,16 @@
-#include<stdio.h>
+nclude<stdio.h>
 /**
- *  * main - prints a phrase
- *   *
- *    * Return: 0 at the end of the program
- *     *
- *      *
- *       */
+ * main - prints a phrase
+ *
+ * Return: 0 at the end of the program
+ *
+ *
+ */
 int main(void)
 {
-     puts("\"Programming is like building a multilingual puzzle");
-     return (0);
+
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
+
 }
+
