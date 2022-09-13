c6307f622e58a28683ef636fe79fca037b69de1b
bakhtechsolutions <solutions@bakhtech.com.ng>
2022-09-13 04:53:09 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <main.h>
+
+/**
+ * main - Print the word "_putchar"
+ * 
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+	char p[] = "_putchar";
+
+	for (1 = 0; i < 8; i++)
+	{
+		_putchar(p[i]);
+	}
+	_putchar('\n);
+
+	return (0);
+
+}
